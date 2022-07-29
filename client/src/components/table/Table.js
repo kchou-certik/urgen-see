3334f7fc5593b5c71de15c47877be710094ada74
kchou <kchou94@gmail.com>
2022-07-29 01:17:53 -0400

    add check for empty table so it doesn't break the page

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -5,9 +5,20 @@ import MUITable from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableContainer from '@mui/material/TableContainer';
 import Paper from '@mui/material/Paper';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 
 function Table({ cols, rows, updatable, pKey }) {
     // TODO: might need to remove this. Checks if we're using "hardcoded" table header names/data
+    if (rows.length === 0) {
+        return (
+            <Alert severity="info">
+                <AlertTitle>No Data</AlertTitle>
+                There are currently no entries in this table.
+            </Alert>
+        )
+    }
+
     cols = isNaN(Object.keys(rows[0])[0]) ? Object.keys(rows[0]) : cols;
 
     return (
