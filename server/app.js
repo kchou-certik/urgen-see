2974cb3311e6deea410d6920a8b8a2f5c8911cf8
kchou <kchou94@gmail.com>
2022-07-31 18:35:30 -0400

    modularize routing

--- server/app.js
+++ server/app.js
@@ -2,82 +2,23 @@ require('dotenv').config();
 const db = require('./db');
 const cors = require('cors');
 
-// Express:
+
+// Express initialization
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/carriers', (req, res) => {
-    db.pool.query('SELECT * FROM Carriers;', (err, rows, fields) => {
-        if (err) {
-            res.status(500).send(err);
-            return;
-        }
-        res.json(rows);
-    });
-});
+// Routes
+const carriers = require('./routes/carriers');
 
-app.get('/carriers/:id', (req, res) => {
-    db.pool.query('SELECT * FROM Carriers WHERE carrier_ID=?;', req.params.id, (err, rows, fields) => {
-        if (err) {
-            res.status(500).send(err);
-            return;
-        }
-        res.json(rows[0]);  // SELECT returns an array; we only need the first result
-    });
-});
+app.use('/carriers', carriers);
 
-app.post('/carriers', (req, res) => {
-    const { phone_number, provider } = req.body;
-    if (!phone_number || !provider) {
-        res.sendStatus(400);
-        return;
-    }
-    db.pool.query(`INSERT INTO Carriers (phone_number, provider)
-        VALUES (?, ?)`,
-        [phone_number, provider],
-        (err, rows, fields) => {
-            if (err) {
-                res.status(500).json(err);
-            } else {
-                res.json(rows);
-            }
-        });
-});
 
-app.delete('/carriers/:id', (req, res) => {
-    const carrier_id = req.params.id;
-    db.pool.query('DELETE FROM Carriers WHERE carrier_id = ?', carrier_id, (err, results, fields) => {
-        if (err) {
-            res.status(500).json(err);
-        } else {
-            res.json(results);
-        }
-    });
-});
-
-app.put('/carriers/:carrier_ID', (req, res) => {
-    const { provider, phone_number } = req.body;
-    const carrier_ID = req.params.carrier_ID;
-    if (!phone_number || !provider) {
-        res.sendStatus(400);
-        return;
-    }
-    let updateCarrierQuery = `UPDATE Carriers SET phone_number = ?, provider = ? WHERE carrier_ID = ?`;
-    db.pool.query(updateCarrierQuery, [phone_number, provider, carrier_ID],
-        (err, rows, fields) => {
-            if (err) {
-                console.log(err);
-                res.sendStatus(500);
-            } else {
-                res.send(rows);
-            }
-        });
-});
 
 app.listen(port, () => {
     console.log(`API server started on port ${port}`);
